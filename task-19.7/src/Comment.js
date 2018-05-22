66b3511d83883b2eaaad63815efcacf45099eef9
Adam Pawliński <adam.pawlinski@gmail.com>
2018-05-22 05:59:20 +0200

    thumb up action created, bugs fixed, app works properly

--- task-19.7/src/Comment.js
+++ task-19.7/src/Comment.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import thumbUpComment from './actions';
+import thumbDownComment from './actions';
 
-const Comment = ({text, votes, id, thumbUpComment}) => <li>{text} <span>votes: {votes}</span><button onClick={() => thumbUpComment(id)}>Thumb up</button></li>;
+
+const Comment = ({text, votes, id, thumbUpComment, thumbDownComment}) => <li>{text} <br/> <span>votes: {votes}</span><br/><button onClick={() => thumbUpComment(id)} className="buttonUp">Thumb up</button><button onClick={() => thumbDownComment(id)} className="buttonDown">Thumb down</button></li>;
 
 export default Comment;
